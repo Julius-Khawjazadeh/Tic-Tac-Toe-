a7bc48265654df4469065cbf0ac28dbdf9a5b221
Julius Khawjazadeh <juliuskhawjazadeh@Juliuss-MacBook-Pro.local>
2022-03-09 23:56:39 -0500

    Implemented logic to check whether a player has won or if the game was a tie

--- Main.py
+++ Main.py
@@ -11,6 +11,12 @@ def printBoard(b):
     print('-+-+-')
     print(b['1'] + '|' + b['2'] + '|' + b['3'])
 
+def printResult(board, turn):
+    printBoard(board)
+    print("\nGame Over.")
+    print("---------------------")
+    print("*** Player " + turn + " won! ***\n") 
+
 def game():
     counter = 0
     turn = 'X' 
@@ -26,6 +32,40 @@ def game():
         else:
             print("That slot is already filled. Please try again: ")
             continue
+
+        # Checking to see if a player has won
+        if board['7'] == board['8'] == board['9'] != ' ': # Across the top
+            printResult(board, turn)
+            break
+        elif board['4'] == board['5'] == board['6'] != ' ': # Across the middle
+            printResult(board, turn)
+            break
+        elif board['1'] == board['2'] == board['3'] != ' ': # Across the bottom
+            printResult(board, turn)
+            break
+        elif board['7'] == board['4'] == board['1'] != ' ': # Down the left side
+            printResult(board, turn)
+            break
+        elif board['8'] == board['5'] == board['2'] != ' ': # Down the middle 
+            printResult(board, turn)
+            break
+        elif board['9'] == board['6'] == board['3'] != ' ': # Down the right side 
+            printResult(board, turn)
+            break
+        elif board['1'] == board['5'] == board['9'] != ' ': # Diagonal 
+            printResult(board, turn)
+            break
+        elif board['7'] == board['5'] == board['3'] != ' ': # Diagonal 
+            printResult(board, turn)
+            break
+            
+        if counter == 9: # It's a tie!
+            print("\nGame Over.")
+            print("---------------------")
+            print("The game is a tie!")
+            break 
+
+
 
         # Switching turns
         if turn == 'X':
